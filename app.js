282035b39e37a0899270ac2270b09dd6e744851c
jflorimo <flojeanc@yahoo.fr>
2015-10-04 16:44:18 +0200

    evol(): adding parsing inputs

--- app.js
+++ app.js
@@ -1,33 +1,31 @@
 var canvas = document.getElementById("renderCanvas");
 var engine = new BABYLON.Engine(canvas, true);
+var scene = new BABYLON.Scene(engine);
 
-var molecules = [];
-
-var initSceneCameraAndLight = function ()
-{
-	var scene = new BABYLON.Scene(engine);
+var initSceneCameraAndLight = function(){
 	var camera = new BABYLON.FreeCamera("camera1", new BABYLON.Vector3(0, 5, -10), scene);
 	camera.setTarget(BABYLON.Vector3.Zero());
 	camera.attachControl(canvas, true);
 	var light = new BABYLON.HemisphericLight("light1", new BABYLON.Vector3(0, 1, 0), scene);
 	light.intensity = 0.7;
-}
-
-var createMol = function (radius, x, y, z)
-{
-	var sphere = BABYLON.Mesh.CreateSphere("sphere1", 16, radius, scene);
-
-}
-
-var createScene = function () 
-{
-	
-	sphere.position.y = 1;
-	var ground = BABYLON.Mesh.CreateGround("ground1", 6, 6, 2, scene);
-	return scene;
 };
 
-var scene = createScene();
+var createAtom = function(size, x, y , z){
+	var sphere = BABYLON.Mesh.CreateSphere("atom", 16, size, scene);
+	sphere.position.x = x;
+	sphere.position.y = y;
+	sphere.position.z = z;
+};
+
+var createScene = function() {
+	initSceneCameraAndLight();
+	// var sphere = BABYLON.Mesh.CreateSphere("sphere1", 16, 2, scene);
+	// sphere.position.y = 1;
+	// var ground = BABYLON.Mesh.CreateGround("ground1", 6, 6, 2, scene);
+	createAtom(1, 1.2, 1.2, 1.5);
+};
+
+createScene();
 
 engine.runRenderLoop(function () {
     scene.render();
